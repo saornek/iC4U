a38b07c3544154c60469a13b79bac5ac8de3dffd
Selin Alara Ornek <47495313+saornek@users.noreply.github.com>
2023-11-17 04:05:29 -0500

    *updated door guide project codes
    Added sound guidelines.

--- iC4U3/codes/door guide project /cv_viewer/exporter.py
+++ iC4U3/codes/door guide project /cv_viewer/exporter.py
@@ -1,35 +1,105 @@
 """
 Author: saornek
 Date: 08/04/2023
-Last Updated: 11/14/2023
-Status: Working
+Last Updated: 11/17/2023
+Status: Testing --> Sound Guidlines
 Purpose: iC4U3 Door Guide Project - Give distance between a visually impaired person's hand and the door knob.
-Update: Simplified calculation with 2D position values. Added return, so the directions can be printed to the screen.
-Notes: Next step voice commands.
+Update: Added sound guidelines.
+Notes:
+For testing purposes used the pygame.mixer library for audio output as it doesn't freeze the camera output.
+Needs cleaning and formatting
 """
 
 # Import Libraries
-import serial 
+import serial
+from time import sleep
 
+# Import for TTS - TEST PURPOSES ONLY - Uncomment to test
+from gtts import gTTS
+from pygame import mixer
+
+mixer.init()
 #ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=3) #comment for test purposes
 
-def values(x_value, y_value):
-    global y_read
-    errorCalculation = 60
+prev_y_read = ""
+prev_x_read = ""
+sayStatus = 0
+repeatBlock = 0
 
+def values(x_value, y_value):
+    global y_read, x_read
+    global prev_y_read, prev_x_read
+    global sayStatus, repeatBlock
+    errorCalculation = 60
+    
+    # Y-AXIS Calculation (Up-Down)
     if y_value > errorCalculation:
         y_read = "UP"
     elif y_value < -errorCalculation:
         y_read = "DOWN"
     else:
-        y_read = "Y AXIS OK."
+        y_read = "Y AXIS OK"
 
+    if ((y_read != prev_y_read) and (y_read != "Y AXIS OK") and (sayStatus == 0)):
+        say(y_read)
+        sayStatus = 0
+        prev_y_read = y_read
+    elif ((y_read != prev_y_read) and (y_read == "Y AXIS OK") and (sayStatus == 0) and (repeatBlock == 0)):
+         say("Your hand is correct vertically.")
+         repeatBlock += 1
+         prev_y_read = y_read 
+         sayStatus = 1
+    elif ((y_read == "Y AXIS OK") and (sayStatus == 1) and (repeatBlock > 0)):
+            if (repeatBlock == 25):
+                sayStatus = 2
+                repeatBlock = 0
+            else:
+                repeatBlock += 1
+
+    # X-AXIS Calculation (Left-Right)
     if x_value > errorCalculation:
         x_read = "LEFT"
     elif x_value < -errorCalculation:
         x_read = "RIGHT"
     else:
-        x_read = "X AXIS OK."
+        x_read = "X AXIS OK" 
+
+    if ((x_read != prev_x_read) and (x_read != "X AXIS OK") and (sayStatus == 2)):
+        say(x_read)
+        sayStatus = 2
+        prev_x_read = x_read
+    elif ((x_read != prev_x_read) and (x_read == "X AXIS OK") and (sayStatus == 2)):
+        say("Your hand is correct horizontaly.")
+        sayStatus = 3
+        prev_x_read = x_read
+    elif ((y_read == "Y AXIS OK") and (sayStatus == 3) and (repeatBlock > 0)):
+        if (repeatBlock == 20):
+            sayStatus = 4
+            repeatBlock = 0
+        else:
+            repeatBlock += 1
 
     return str("Y AXIS: " + y_read + " / X AXIS: " + x_read)
-        
+    
+
+    """if ((y_read == "Y AXIS OK") and (x_read == "X AXIS OK") and (sayStatus == 4)):
+        say("Your hand should be on the knob. You are ready to open the door.")
+        sayStatus = 5
+        return str("Y AXIS: Correct / X AXIS: Correct.")
+    else:
+        return str("Y AXIS: " + y_read + " / X AXIS: " + x_read)
+    """
+    
+    # print("Y AXIS:", y_read, "X AXIS", x_read) #Uncomment to test.
+
+def say(text):
+    # Create a gTTS object with the text
+    tts = gTTS(text=text, lang='en')
+
+    # Save the speech audio to a file
+    audio_file = 'output.mp3'
+    tts.save(audio_file)
+
+    # Play the audio file
+    sound = mixer.Sound('output.mp3')
+    sound.play()
